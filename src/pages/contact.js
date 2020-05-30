f4976cd6d327596386f6f096098494f311318b05
jjeong1 <jjeong1@andrew.cmu.edu>
2020-05-30 16:27:10 -0700

    leave contcat form component floating

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import ContactSection from "../components/sections/contact-section";
 
 const ContactPage = () => {
   return (
     <Layout title="Contact">
       <SEO title="Contact" />
-      <ContactSection />
     </Layout>
   );
 };
