70c2bf55a15c00dd9a61ad5aed9e24b6e03400d0
Alex Gyujin Kim <alexgkim205@gmail.com>
2020-05-30 19:17:11 -0400

    buttons work now

--- src/components/sections/button-section.js
+++ src/components/sections/button-section.js
@@ -6,87 +6,85 @@ import { Link } from "gatsby";
 import BaseSection from "./base.style";
 import {
   WHITE,
-  BOX_SHADOW,
-  BLACK,
   BRIGHT_PURPLE,
-  LIGHT_GRAY,
   BUTTON_SHADOW,
-  breakpoint
+  breakpoint,
 } from "../../constants/theme";
 
 const Container = styled(BaseSection)`
-	flex-direction: row;
-	justify-content: space-evenly;
-	flex-wrap: wrap;
+  .content {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+  }
 `;
 
 const Button = styled(Link)`
-	display: flex;
-	background-color: ${BRIGHT_PURPLE};
-	box-sizing: border-box;
-	align-items: center;
-	border-radius: 4px;
-	box-shadow: ${BUTTON_SHADOW};
-	color: ${WHITE};
-	padding: 1rem 2rem;
-	font-weight: normal;
+  background-color: ${BRIGHT_PURPLE};
+  border-radius: 3px;
+  box-shadow: ${BUTTON_SHADOW};
+  color: ${WHITE};
+  padding: 1rem 2rem;
 	text-decoration: none;
+	transition: 0.1s all;
 
-	&:hover {
+  &:hover {
     transform: translate(0, 1px);
     text-decoration: none;
     box-shadow: none;
-    text-decoration: underline;
-	}
+  }
 `;
 
 const ButtonContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	.message {
-		font-weight: bold;
-		margin-bottom: 1rem;
-	}
-	${breakpoint.down("m")`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: "Poppins", sans-serif;
+  font-weight: 500;
+
+  .message {
+    margin-bottom: 1rem;
+  }
+  ${breakpoint.down("m")`
 		flex: 1 0 50%;
 		margin-bottom: 2rem;
 		.message {
 			margin-bottom: 1rem;
 		}
 	`}
-	${breakpoint.down("xs")`
+  ${breakpoint.down("s")`
 		flex: 1 0 100%;
 	`}
 `;
 
-function GetButtons(item) {
-	const buttons = item.buttons.map((button, index) =>
-		<ButtonContainer>
-			<p className="message">{button.message}</p>
-			<Button key={index} to={button.link} active="active">
-				{button.buttonTitle}
-			</Button>
-		</ButtonContainer>
-	);
-	return (
-		<Fragment>
-			{buttons}
-		</Fragment>
-	)
-}
-
-const ButtonSection = ({buttons}) => {
-	return (
-		<Container>
-			<GetButtons buttons={buttons}/>
-		</Container>
-	);
+const ButtonSection = ({ buttons }) => {
+  return (
+    <Container>
+      <div className="content">
+        {buttons.map(({ message, link, buttonTitle }, index) => (
+          <ButtonContainer>
+            <p className="message">{message}</p>
+            <Button key={index} to={link} active="active">
+              {buttonTitle}
+            </Button>
+          </ButtonContainer>
+        ))}
+      </div>
+    </Container>
+  );
 };
 
 ButtonSection.propTypes = {
-	buttons: PropTypes.arrayOf(PropTypes.object)
-}
+  buttons: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string,
+      buttonTitle: PropTypes.string,
+      link: PropTypes.string,
+    })
+  ).isRequired,
+};
 
 export default ButtonSection;
