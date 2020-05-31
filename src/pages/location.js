fceef9f54d217f4ba36d57de932bf7e6ba07bd08
jjeong1 <jjeong1@andrew.cmu.edu>
2020-05-31 14:55:09 -0700

    added responsivity

--- src/pages/location.js
+++ src/pages/location.js
@@ -5,10 +5,50 @@ import Img from "gatsby-image";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import BaseSection from "../components/sections/base.style"
+import Button from "../constants/button";
+import ContainedSection from "../components/sections/contained-section";
+import { 
+	breakpoint,
+	WHITE
+} from "../constants/theme";
 
 const LocationContainer = styled.div`
 	display: flex;
-	justify-content: center;
+	justify-content: space-evenly;
+	align-items: stretch;
+	padding: 2em;
+
+  ${breakpoint.down("m")`
+		flex-direction: column;
+		padding: 1.5em;
+	`}
+`
+
+const AddressBox = styled.div`
+  font-family: "Poppins", sans-serif;
+  width: 40%;
+  height: 100%;
+  padding: 1em;
+	.title {
+		font-weight: bold;
+		&:first-child {
+			margin-top: 0.5em;
+		}
+	}
+	.oppositeAlign {
+		display: flex;
+		justify-content: space-between;
+	}
+
+  ${breakpoint.down("m")`
+		width: 100%;
+	`}
+`;
+
+const ButtonContainer = styled(BaseSection)`
+	padding: 1em;
+	margin-bottom: 1em;
 `;
 
 const LocationPage = () => {
@@ -27,15 +67,41 @@ const LocationPage = () => {
   return (
     <Layout title="Location">
       <SEO title="Location"/>
-      <LocationContainer>
-	      <Img
-	      	fluid={data.logoImage.childImageSharp.fluid}
-	      	alt="location"
-	      	style={{
-	      		width: "40rem",
-	      	}}
-	      />
-      </LocationContainer>
+      <ContainedSection>
+	      <LocationContainer>
+		      <Img
+		      	fluid={data.logoImage.childImageSharp.fluid}
+		      	alt="location"
+		      	style={{
+		      		width: "100%",
+		      		height: "100%",
+		      		margin: "1.5em"
+		      	}}
+		      />
+		      <AddressBox>
+		      	<p className="title">Address</p>
+		      	<p>VnS Academy<br />
+		      		211-63 46th Ave FL 1<br />
+		      		Queens, NY<br />
+		      		11361
+	      		</p>
+		      	<p className="title">Hours</p>
+		      	<div className="oppositeAlign">
+			      	<div>MTWThF</div>
+		      		<div>8AM - 5PM</div>	
+		      	</div>
+		      	<div className="oppositeAlign">
+			      	<div>Sat+Sun</div>
+			      	<div>10AM - 7PM</div>
+			      </div>
+		      </AddressBox>
+	      </LocationContainer>
+	      <ButtonContainer>
+		      <Button to={dirLink} active="active" className="button">
+		    		Get Directions
+		  		</Button>
+	  		</ButtonContainer>
+  		</ContainedSection>
     </Layout>
   );
 };
