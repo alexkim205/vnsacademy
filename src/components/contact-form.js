d05c78a811eda2a5158663aa35ca3df1d50cb86b
Alex Gyujin Kim <alexgkim205@gmail.com>
2020-05-31 18:26:10 -0400

    most of form fields ready

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useForm, ErrorMessage } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import axios from "axios";
-import styled from "styled-components";
 import { formatPhoneNumber } from "react-phone-number-input";
 import PhoneInput from "react-phone-number-input/input";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import BaseSection from "./sections/base.style";
-import {
-  WHITE,
-  BUTTON_SHADOW,
-  BOX_SHADOW,
-  SUBJECTS_COLORS,
-  breakpoint,
-} from "../constants/theme";
+import { Container, Select } from "./contact-form.style";
 
 const emailAddress = "vnsacademy@gmail.com";
 const ccEmailAddresses = "sogyu30@yahoo.com,oliviaebea@yahoo.com";
@@ -28,26 +20,6 @@ const toastOptions = {
   draggable: false,
   progress: undefined,
 };
-
-const Container = styled(BaseSection)`
-  .content {
-    width: 100%;
-    box-sizing: border-box;
-    background-color: ${WHITE};
-    align-items: center;
-    border-radius: 5px;
-    box-shadow: ${BOX_SHADOW};
-    padding: 3.5em 2.5em;
-
-    form {
-      display: flex;
-      flex-direction: column;
-
-      .submit {
-      }
-    }
-  }
-`;
 
 const ContactForm = () => {
   const { register, handleSubmit, setValue, getValues, errors } = useForm();
@@ -112,58 +84,100 @@ const ContactForm = () => {
     <Container>
       <div className="content">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <select
-            name="reason"
-            ref={register({ required: "Reason is required." })}
-          >
-            <option value="registering a student for a group class">
-              registering a student for a group class
-            </option>
-            <option value="registering a student for private tutoring">
-              registering a student for private tutoring
-            </option>
-            <option value="becoming a tutor">becoming a tutor</option>
-            <option value="other">other</option>
-          </select>
-          <div className="input-error">{errors?.reason?.message}</div>
-          <select
-            name="subject"
-            ref={register({ required: "Subject is required." })}
-          >
-            <option value="SAT">SAT</option>
-            <option value="SAT Subject Tests">SAT Subject Tests</option>
-            <option value="Biology">Biology</option>
-            <option value="Chemistry">Chemistry</option>
-            <option value="Physics">Physics</option>
-            <option value="Math">Math</option>
-            <option value="SHSAT">SHSAT</option>
-            <option value="Hunter Entrance Exam">Hunter Entrance Exam</option>
-            <option value="Competitive Math">Competitive Math</option>
-            <option value="School Math Prep">School Math Prep</option>
-            <option value="School English Prep">School English Prep</option>
-          </select>
-          <div className="input-error">{errors?.subject?.message}</div>
-          <PhoneInput
-            country="US"
-            name="phone"
-            value={phoneValue}
-            onChange={value => {
-              setValue("phone", value);
-              setPhoneValue(value);
-            }}
-          />
-          <div className="input-error">{errors?.phone?.message}</div>
-          <input
-            type="text"
-            name="email"
-            ref={register({
-              required: "Email is required.",
-              pattern: { value: /^\S+@\S+$/i, message: "Email is invalid." },
-            })}
-          />
-          <div className="input-error">{errors?.email?.message}</div>
-          <textarea name="moreinfo" ref={register} />
-          <div className="input-error">{errors?.moreinfo?.message}</div>
+          <div className="input-wrapper">
+            <div className="input-text">You are contacting us about</div>{" "}
+            <div className="input-input">
+              <Select
+                name="reason"
+                ref={register({ required: "Reason is required." })}
+              >
+                <option value="registering a student for a group class">
+                  registering a student for a group class
+                </option>
+                <option value="registering a student for private tutoring">
+                  registering a student for private tutoring
+                </option>
+                <option value="becoming a tutor">becoming a tutor</option>
+                <option value="other">other</option>
+              </Select>
+            </div>
+            <div className="input-text append">,</div>
+            {/* <div className="input-error">{errors?.reason?.message}</div> */}
+          </div>
+          <div className="input-wrapper">
+            <div className="input-text">for</div>{" "}
+            <div className="input-input">
+              <Select
+                name="subject"
+                ref={register({ required: "Subject is required." })}
+              >
+                <option value="SAT">the SAT</option>
+                <option value="SAT Subject Tests">the SAT Subject Tests</option>
+                <option value="Biology">Biology</option>
+                <option value="Chemistry">Chemistry</option>
+                <option value="Physics">Physics</option>
+                <option value="Math">Math</option>
+                <option value="SHSAT">the SHSAT</option>
+                <option value="Hunter Entrance Exam">
+                  the Hunter Entrance Exam
+                </option>
+                <option value="Competitive Math">Competitive Math</option>
+                <option value="School Math Prep">School Math Prep</option>
+                <option value="School English Prep">School English Prep</option>
+              </Select>
+            </div>
+            <div className="input-text append">.</div>
+            {/* <div className="input-error">{errors?.subject?.message}</div> */}
+          </div>
+          <div className="input-wrapper">
+            <div className="input-text">I'd like to be reached at</div>{" "}
+            <div className="input-input">
+              <PhoneInput
+                country="US"
+                name="phone"
+                placeholder="(123) 456-7890"
+                value={phoneValue}
+                onChange={value => {
+                  setValue("phone", value);
+                  setPhoneValue(value);
+                }}
+              />{" "}
+            </div>
+            <div className="input-text">or</div>{" "}
+            <div className="input-input">
+              <input
+                type="text"
+                placeholder="user@email.com"
+                name="email"
+                ref={register({
+                  required: "Email is required.",
+                  pattern: {
+                    value: /^\S+@\S+$/i,
+                    message: "Email is invalid.",
+                  },
+                })}
+              />{" "}
+            </div>
+            <div className="input-text">.</div>
+            <div className="input-error">{errors?.phone?.message}</div>
+            <div className="input-error">{errors?.email?.message}</div>
+          </div>
+          <div className="input-wrapper">
+            <div className="input-text" style={{ padding: "0.7em 0" }}>
+              Any additional information:
+            </div>
+            <div className="input-input break">
+              {" "}
+              <textarea
+                name="moreinfo"
+                ref={register}
+                rows={5}
+                spellCheck={false}
+                
+              />
+            </div>
+            <div className="input-error">{errors?.moreinfo?.message}</div>
+          </div>
           <input type="submit" />
         </form>
       </div>
---
+++ src/components/contact-form.style.js
@@ -0,0 +1,142 @@
+import React from "react";
+import styled from "styled-components";
+
+import BaseSection from "./sections/base.style";
+import {
+  WHITE,
+  BUTTON_SHADOW,
+  BOX_SHADOW,
+  SUBJECTS_COLORS,
+  DARK_BACKGROUND_WHITE,
+  breakpoint,
+  MEDIUM_GRAY,
+  BACKGROUND_WHITE,
+} from "../constants/theme";
+
+export const Container = styled(BaseSection)`
+  .content {
+    width: 100%;
+    box-sizing: border-box;
+    background-color: ${WHITE};
+    align-items: center;
+    border-radius: 5px;
+    box-shadow: ${BOX_SHADOW};
+    padding: 3.5em 2.5em;
+
+    form {
+      display: flex;
+      flex-direction: column;
+      font-family: "Poppins", sans-serif;
+      font-size: 1em;
+
+      .input-wrapper {
+        display: block;
+        margin-bottom: 1em;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+
+        .input-text {
+          display: inline-block;
+          font-weight: 500;
+
+          &.append {
+            margin-left: 1em;
+          }
+        }
+        .input-input {
+          display: inline-block;
+
+          &.break {
+            display: block;
+          }
+
+          input {
+            background-color: ${DARK_BACKGROUND_WHITE};
+            border: 1px solid ${DARK_BACKGROUND_WHITE};
+            border-radius: 4px;
+            // width: 100%;
+            padding: 0.7em 0.5em;
+            margin: 0 0.5em;
+            font-size: 1.05em;
+            color: #3f3f3f;
+
+            &[name="phone"] {
+              width: 110px;
+              // text-align: center;
+            }
+            &[name="email"] {
+              width: 180px;
+            }
+          }
+
+          textarea {
+            font-family: "Poppins", sans-serif;
+            font-size: 1.05em;
+            background: ${DARK_BACKGROUND_WHITE};
+            border: 1px solid ${DARK_BACKGROUND_WHITE};
+            border-radius: 4px;
+            padding: 0.7em 0.5em;
+            margin: 0.5em 0;
+            width: 100%;
+            box-sizing: border-box;
+            resize: none;
+          }
+        }
+
+        .input-error {
+          width: 100%;
+        }
+      }
+
+      .submit {
+      }
+    }
+  }
+`;
+
+const SelectContainer = styled.div`
+  position: relative;
+
+  &:after {
+    content: ">";
+    transform: rotate(90deg);
+    font-size: 1.3em;
+    position: absolute;
+    top: 0.5em;
+    right: 0.3em;
+    color: #3f3f3f;
+    pointer-events: none;
+  }
+
+  select {
+    background-color: ${DARK_BACKGROUND_WHITE};
+    border: 1px solid ${DARK_BACKGROUND_WHITE};
+    border-radius: 4px;
+    width: 100%;
+    padding: 0.7em 1.2em 0.7em 0.5em;
+    margin: 0 0.5em;
+    font-size: 1.05em;
+    color: #3f3f3f;
+
+    /* Here's the code we need */
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -ms-appearance: none;
+    -o-appearance: none;
+    appearance: none;
+
+    &::-ms-expand {
+      display: none;
+    }
+  }
+`;
+
+export const Select = ({ children, ...selectProps }) => {
+  return (
+    <SelectContainer>
+      <select {...selectProps}>{children}</select>
+    </SelectContainer>
+  );
+};
