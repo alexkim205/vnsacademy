a6e7e3fed2d8561bb708627739623d757284d192
jjeong1 <jjeong1@andrew.cmu.edu>
2020-05-31 15:13:34 -0700

    fixed broken linking to google maps url

--- src/pages/location.js
+++ src/pages/location.js
@@ -8,7 +8,12 @@ import SEO from "../components/seo";
 import BaseSection from "../components/sections/base.style"
 import Button from "../constants/button";
 import ContainedSection from "../components/sections/contained-section";
-import { breakpoint } from "../constants/theme";
+import { 
+	breakpoint,
+	WHITE,
+	BRIGHT_PURPLE,
+	BUTTON_SHADOW
+} from "../constants/theme";
 
 const LocationContainer = styled.div`
 	display: flex;
@@ -46,6 +51,21 @@ const AddressBox = styled.div`
 const ButtonContainer = styled(BaseSection)`
 	padding: 1em;
 	margin-bottom: 1em;
+	a {
+	  background-color: ${BRIGHT_PURPLE};
+	  border-radius: 3px;
+	  box-shadow: ${BUTTON_SHADOW};
+	  color: ${WHITE};
+	  padding: 1rem 2rem;
+		text-decoration: none;
+		transition: 0.1s all;
+
+	  &:hover {
+	    transform: translate(0, 1px);
+	    text-decoration: none;
+	    box-shadow: none;
+	  }
+	}
 `;
 
 const LocationPage = () => {
@@ -94,9 +114,9 @@ const LocationPage = () => {
 		      </AddressBox>
 	      </LocationContainer>
 	      <ButtonContainer>
-		      <Button to={dirLink} active="active" className="button">
+		      <a href={dirLink} active="active">
 		    		Get Directions
-		  		</Button>
+		  		</a>
 	  		</ButtonContainer>
   		</ContainedSection>
     </Layout>
