d042f9f178629269e31e0f42c43c09c94ace3596
Alex Kim <alexgkim205@gmail.com>
2020-12-24 11:56:58 -0500

    Revert "updated classes data and added student info forms in contact form"
    This reverts commit 19e05f6475079b166d2ead7caf38f6e63d35f56c.

--- src/pages/contact-hire.js
+++ src/pages/contact-hire.js
@@ -14,7 +14,6 @@ const ContactHirePage = ({ location }) => {
         reason={location?.state?.reason}
         reasons={HIRING_REASONS}
         subjects={HIRING_SUBJECTS}
-        isContactPage={false}
       />
     </Layout>
   );
